b4fd533800100dddf8a11c4cd9760fa2027f14e2
pavan6717624 <pvn.kumar4@gmail.com>
2022-12-31 23:05:52 +0530

    deploy

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -9,7 +9,34 @@ import { DeviceDetectorService } from 'ngx-device-detector';
 export class HomeComponent implements OnInit {
 
   constructor(private deviceService: DeviceDetectorService) { }
-  menuItems=[{label:'Home'},{label:'Contact us'},{label:'Login / Sign Up'}];
+  menuItems=[{label:'Home',icon: 'pi pi-home'},{label:'About us',icon: 'pi pi-book'},{label:'Services',icon: 'pi pi-flag',
+
+  items: [{
+    label: 'ANDT Services', 
+    icon: 'pi pi-slack',
+  },
+  {
+    label: 'CNDT Services', 
+    icon: 'pi pi-hourglass',
+  },
+  {
+    label: 'Manufacturing Services', 
+    icon: 'pi pi-shield',
+  },
+  {
+    label: 'Training Services', 
+    icon: 'pi pi-key',
+  },
+  {
+    label: 'Manpower Supply', 
+    icon: 'pi pi-users',
+  },
+  {
+    label: 'Industries', 
+    icon: 'pi pi-bolt',
+  }],
+
+}, {label:'Contact Us',icon: 'pi pi-phone'}];
   isMobile=false;
   
   ngOnInit(): void {
