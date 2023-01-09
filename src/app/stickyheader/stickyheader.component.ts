b0629e5050956ded437fa1ee65a69f6c2f3e3fbc
pavan6717624 <pvn.kumar4@gmail.com>
2023-01-09 20:51:20 +0530

    deploy

--- src/app/stickyheader/stickyheader.component.ts
+++ src/app/stickyheader/stickyheader.component.ts
@@ -10,43 +10,84 @@ import { DeviceDetectorService } from 'ngx-device-detector';
 export class StickyheaderComponent implements OnInit {
 
   constructor(private deviceService: DeviceDetectorService, private route: Router) { }
-  menuItems=[{label:'Home',icon: 'pi pi-home',  routerLink: 'home'},{label:'About us',icon: 'pi pi-book',  routerLink: 'about'},{label:'Services',icon: 'pi pi-flag',
+  menuItems = [{ label: 'Home', icon: 'pi pi-home', routerLink: 'home' }, {
+    label: 'Services', icon: 'pi pi-flag',
 
-  items: [{
-    label: 'ANDT Services', 
-    icon: 'pi pi-slack',  routerLink: 'andts'
-   
-  },
-  {
-    label: 'CNDT Services', 
-    icon: 'pi pi-hourglass',  routerLink: 'cndts'
-  },
-  {
-    label: 'Manufacturing Services', 
-    icon: 'pi pi-shield',  routerLink: 'manufacturingservices'
-  },
-  {
-    label: 'Training Services', 
-    icon: 'pi pi-key',  routerLink: 'trainingservices'
-  },
-  {
-    label: 'Manpower Supply', 
-    icon: 'pi pi-users',  routerLink: 'manpowersupply'
-    
-  },
-  {
-    label: 'Industries', 
-    icon: 'pi pi-bolt',  routerLink: 'industries'
-  }],
+    items: [{
+      label: 'ANDT Services',
+      icon: 'pi pi-slack', routerLink: 'andts',
 
-}, {label:'Contact Us',icon: 'pi pi-phone',  routerLink: 'contact'}];
-isMobile=false;
+      items: [{
+        label: 'Tube Inspection Services',
+        icon: 'pi pi-angle-right', routerLink: 'tubeinspectionservices'
+      },
+      {
+        label: '3D MFL Tank Floor Scanning',
+        icon: 'pi pi-angle-right', routerLink: 'mfltankfloorscanning'
+      },
+      {
+        label: 'PHASED ARRAY',
+        icon: 'pi pi-angle-right', routerLink: 'phasedarray'
+      },
+      {
+        label: 'TIME OF FLIGHT DIFFRACTION (TOFD)',
+        icon: 'pi pi-angle-right', routerLink: 'timeofflightdiffraction'
+      },
+      {
+        label: 'Borescope Inspections',
+        icon: 'pi pi-angle-right', routerLink: 'borescopeinspections'
+      }]
+
+
+    },
+    {
+      label: 'CNDT Services',
+      icon: 'pi pi-hourglass', routerLink: 'cndts',
+
+      items: [{
+        label: 'Magnetic Particle Inspection',
+        icon: 'pi pi-angle-right', routerLink: 'magneticparticleinspection'
+      },
+      {
+        label: 'Dye Penetrant Inspection',
+        icon: 'pi pi-angle-right', routerLink: 'dyepenetrantinspection'
+      },
+      {
+        label: 'Industrial Radiography',
+        icon: 'pi pi-angle-right', routerLink: 'industrialradiography'
+      },
+      {
+        label: 'Ultrasonic Inspection',
+        icon: 'pi pi-angle-right', routerLink: 'ultrasonicinspection'
+      },
+      ]
+
+    },
+    {
+      label: 'Manufacturing Services',
+      icon: 'pi pi-shield', routerLink: 'manufacturingservices'
+    },
+    {
+      label: 'Training Services',
+      icon: 'pi pi-key', routerLink: 'trainingservices'
+    },
+    {
+      label: 'Manpower Supply',
+      icon: 'pi pi-users', routerLink: 'manpowersupply'
+
+    },
+    {
+      label: 'Industries',
+      icon: 'pi pi-bolt', routerLink: 'industries'
+    }],
+
+  }, { label: 'About us', icon: 'pi pi-book', routerLink: 'about' }, { label: 'Contact Us', icon: 'pi pi-phone', routerLink: 'contact' }];
+  isMobile = false;
   ngOnInit(): void {
     this.isMobile = this.deviceService.isMobile();
   }
 
-  toHome()
-  {
+  toHome() {
     this.route.navigate(['home']);
   }
 }
