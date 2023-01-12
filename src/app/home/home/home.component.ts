76a42626cbf5fed42403cf85a3d32ee051a67297
pavan6717624 <pvn.kumar4@gmail.com>
2023-01-12 12:06:15 +0530

    deploy

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
@@ -8,13 +9,21 @@ import { DeviceDetectorService } from 'ngx-device-detector';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
- 
-  
-  
-  ngOnInit(): void {
-  
-  }
+  constructor(  
+    private titleService: Title,  
+    private metaTagService: Meta  
+  ) { }  
 
+  ngOnInit(): void {
+
+    this.titleService.setTitle("VNDTS - Home");  
+    
+    this.metaTagService.addTags([  
+      { name: 'keywords', content: 'VNDTS, Velocity NDT Solutions, ANDT Services, CNDT Services, Manufacturing Services, Training Services, Man Power Supply, Industries, Tube inspection, Eddy current testing, Iris inspection, Remote field testing, Near field testing, MFL tube testing, MFL tank floor inspection, API Services, Ultrasonic testing, Radiography testing, Mpi testing, Penetration testing, Heat exchanger inspection, Boiler inspection, Internal Rotary Inspection System, Magnetic Flux Leakage Testing, Surface profile with STARS technology, PHASED ARRAY, TIME OF FLIGHT DIFFRACTION, Borescope Inspections, Magnetic Particle Inspection, Wet Fluorescent Magnetic Test, Dye Penetrant Inspection, Industrial Radiography' },  
+      { name: 'robots', content: 'index, follow' },  
+      { name: 'writer', content: 'VNDTS' },  
+      { charset: 'UTF-8' }  
+    ]);  
+  }
  
 }
--- src/app/home/phasedarray/phasedarray.component.ts
+++ src/app/home/phasedarray/phasedarray.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-phasedarray',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./phasedarray.component.css']
 })
 export class PhasedarrayComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private titleService: Title,
+    private metaTagService: Meta
+  ) { }
 
   ngOnInit(): void {
-  }
 
+    this.titleService.setTitle("VNDTS - PHASED ARRAY");
+
+    this.metaTagService.addTags([
+      { name: 'keywords', content: 'VNDTS, Velocity NDT Solutions, ANDT Services, CNDT Services, Manufacturing Services, Training Services, Man Power Supply, Industries, Tube inspection, Eddy current testing, Iris inspection, Remote field testing, Near field testing, MFL tube testing, MFL tank floor inspection, API Services, Ultrasonic testing, Radiography testing, Mpi testing, Penetration testing, Heat exchanger inspection, Boiler inspection, Internal Rotary Inspection System, Magnetic Flux Leakage Testing, Surface profile with STARS technology, PHASED ARRAY, TIME OF FLIGHT DIFFRACTION, Borescope Inspections, Magnetic Particle Inspection, Wet Fluorescent Magnetic Test, Dye Penetrant Inspection, Industrial Radiography' },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'writer', content: 'VNDTS' },
+      { charset: 'UTF-8' }
+    ]);
+  }
 }
